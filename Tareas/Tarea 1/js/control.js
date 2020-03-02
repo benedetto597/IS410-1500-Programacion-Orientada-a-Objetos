c60b46dc0f7d313012a48d59a8c4f3ec4d6c9b7c
benedetto597 <ejbg597@gmail.com>
2020-03-01 19:02:35 -0600

    Tarea1 v1.25 Uso de LocalStorage

--- Tareas/Tarea 1/js/control.js
+++ Tareas/Tarea 1/js/control.js
@@ -1,39 +1,53 @@
-var Contenido= [
+var localStorage = window.localStorage;
+var Content;
+
+if (localStorage.getItem("Content")==null){
+    Content= [
     {
-        usuario:"Juan Perez",
+        user:"Juan Perez",
         post:"",
-        fecha:""
+        date:""
     },
     {
-        usuario:"Lorena Gomez",
+        user:"Lorena Gomez",
         post:"",
-        fecha:""
+        date:""
     },
     {
-        usuario:"Ashley Rios",
+        user:"Ashley Rios",
         post:"",
-        fecha:""
+        date:""
     },
     {
-        usuario:"María Godoy",
+        user:"María Godoy",
         post:"",
-        fecha:""
+        date:""
     },
     {
-        usuario:"Pedro Suazo",
+        user:"Pedro Suazo",
         post:"",
-        fecha:""
+        date:""
     }
 
-]
+    ]
+
+    localStorage.setItem("Content",JSON.stringify(Content));
+}else{
+    Content = JSON.parse(localStorage.getItem('Content'));
+    for(let i=0; i<Content.length; i++){
+        if (Content[i].post != ''){
+            createPost(i);
+        }
+    }
+}
 
 function myOnLoad() {
     ShowUsers();
 }
 
 function ShowUsers(){
-    for(let i=0; i<Contenido.length; i++){ 
-        document.getElementById('Users').innerHTML += `<option value='${Contenido[i].usuario}'>${Contenido[i].usuario}</option>`; 
+    for(let i=0; i<Content.length; i++){ 
+        document.getElementById('Users').innerHTML += `<option value='${Content[i].user}'>${Content[i].user}</option>`; 
         };
 }
 
@@ -41,25 +55,62 @@ function ConfirmPost(){
     if (ValidatePost()== false || ValidateDate() == false){
         return false;
     }else{
-        console.log(Contenido);
+        let posUser = searchUser();
+        createPost(posUser);
+        return true;
+    }
+
+}
+
+function createPost(posUser){
         document.getElementById('mainRow').innerHTML += 
         `<div class="col-xl-3 col-lg-3 col-md-5 col-sm-12 col-xs-12 p-4 mx-1" id="main">
             <div class="card p-3">
                     <div class="card-header" style=" padding-left:0; padding-right:0;">
                         <img id="userlogo"src="img/user.jpg" class="img-thumbnail rounded-circle float-left">
                         <div style="padding-top:10px;">
-                        <b class="card-title text-left" style="font-size:15px" >${document.getElementById('Users').value}</b><h6 style="font-size:10px; color: #6a6d72; display: inline;">(${getDate(document.getElementById('date').value)})</h6>
+                        <b class="card-title text-left" style="font-size:15px" >${Content[posUser].user}</b><h6 style="font-size:10px; color: #6a6d72; display: inline;">(${Content[posUser].date})</h6>
                         </div>
                     </div>
             <form id="userPost" class="card-body">
                 <div class="form-group">
-                    <p style="font:">${document.querySelector('textarea').value}</p>
+                    <p style="font:">${Content[posUser].post}</p>
                 </div>
             </form>
             </div>
         </div>`
-    }
+}
 
+function searchUser(){
+    for(let i=0; i<Content.length; i++){
+        if (Content[i].user == document.getElementById('Users').value){
+            return i;
+        }
+    }
+}
+
+function ValidatePost(){
+    if(document.querySelector('textarea').value == ''){
+        document.getElementById('error-post').innerHTML = `<h6>Escriba un Post</h6>`;
+        return false;
+    }
+    let postContent = document.querySelector('textarea').value;
+    let posUser = searchUser();
+    Content[posUser].post += postContent;
+    localStorage.setItem("Content",JSON.stringify(Content));
+    return true; 
+}
+
+function ValidateDate(){
+    if(document.getElementById('date').value == ''){
+        document.getElementById('error-date').innerHTML = `<h6>Seleccione una date</h6>`;
+        return false;
+    }
+    let postDate = document.getElementById('date').value;
+    let posUser = searchUser();
+    Content[posUser].date += getDate(postDate);
+    localStorage.setItem("Content",JSON.stringify(Content));
+    return true; 
 }
 
 function getDate(dateFromUser){
@@ -73,35 +124,6 @@ function getDate(dateFromUser){
             finalDate += month[i] + '/';
         }
     }
-    finalDate += arrayDate[0]+ '';
-    console.log(finalDate);
+    finalDate += arrayDate[0]+ '';  
     return finalDate;
-}
-
-function ValidatePost(){
-    if(document.querySelector('textarea').value == ''){
-        document.getElementById('error-post').innerHTML = `<h6>Escriba un Post</h6>`;
-        return false;
-    }
-    let postContent = document.querySelector('textarea').value;
-    for(let i=0; i<Contenido.length; i++){
-        if (Contenido[i].usuario == document.getElementById('Users').value){
-            Contenido[i].post += postContent;
-            return true;
-        }
-    }
-}
-
-function ValidateDate(){
-    if(document.getElementById('date').value == ''){
-        document.getElementById('error-date').innerHTML = `<h6>Seleccione una Fecha</h6>`;
-        return false;
-    }
-    let postDate = document.getElementById('date').value;
-    for(let i=0; i<Contenido.length; i++){
-        if (Contenido[i].usuario == document.getElementById('Users').value){
-            Contenido[i].fecha += postDate;
-            return true;
-        }
-    }
 }
